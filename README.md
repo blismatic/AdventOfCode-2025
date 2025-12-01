132fd79478cafb232fe48c8a721a0961eb0ac017
Seth Collins <14958849+blismatic@users.noreply.github.com>
2025-12-01 11:55:43 -0800

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # AdventOfCode-2025
+Make sure your Advent of Code session token is stored in a `.env` file
+
+This project uses `uv`, the [extremely fast Python package and project manager](https://docs.astral.sh/uv/).
+
+*Note: last line of input file does **not** end with a newline character*
+
+## Helpful Commands
+`Set-ExecutionPolicy Unrestricted -Scope Process` if you are getting a "*\\AdventOfCode-2025\\.venv\\scripts\\Activate.ps1 cannot be loaded because running scripts is disabled on this system.*" error
+
+`uv self update` to update the UV tool
+
+`uv add X` to install dependency X
+
+`uv remove X` to remove dependecy X
+
+`uv run 01/main.py` to run a file
+
+`uv cache clear` for when you get hardlink errors when trying to add dependencies
---
+++ template.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+
+from aocd import get_data
+from dotenv import load_dotenv
+
+with open("01/example_input.txt", "r") as infile:
+    example_input = infile.read()
+
+
+def parse(puzzle_input: str):
+    """Parse input."""
+    result = puzzle_input.split("\n")
+
+    pprint(result[-3:])
+    print()
+    return result
+
+
+def part1(data):
+    """Solve and return the answer to part 1."""
+    pass
+
+
+def part2(data):
+    """Solve and return the answer to part 2."""
+    pass
+
+
+def solve(puzzle_input) -> tuple:
+    """Solve the puzzle for the given input. Returns a tuple containing the answers to part 1 and part 2."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    solutions = solve(example_input)
+    # puzzle_input = get_data(day=1, year=2025)
+    # solutions = solve(puzzle_input)
+
+    print("\n".join(str(solution) for solution in solutions))
