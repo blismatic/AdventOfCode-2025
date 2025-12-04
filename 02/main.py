8f6fa48c59fd31e53e4963b7230b6d4af2f5d186
Seth Collins <14958849+blismatic@users.noreply.github.com>
2025-12-03 16:06:04 -0800

    solve day 02 part 2

--- 02/main.py
+++ 02/main.py
@@ -1,3 +1,4 @@
+import math
 from pprint import pprint
 
 from aocd import get_data
@@ -11,13 +12,30 @@ The product ID ranges are inclusive, meaning a range of 11-99 *includes* 11 and 
 """
 
 
+def get_factors(n: int) -> list[int]:
+    result = set()
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            result.add(i)
+            result.add(n // i)
+
+    return sorted(result)
+
+
+def generate_n_len_digits(n: int):
+    start = 10 ** (n - 1)
+    end = 10**n
+    for i in range(start, end):
+        yield i
+
+
 def parse(puzzle_input: str) -> list[tuple[int, int]]:
     """Parse input."""
     result = puzzle_input.strip().split(",")
     result = [tuple(map(int, id_range.split("-"))) for id_range in result]
 
-    pprint(result[-3:])
-    print()
+    # pprint(result[-3:])
+    # print()
     return result
 
 
@@ -55,9 +73,34 @@ def part1(data: list[tuple[int, int]]) -> int:
     return sum(invalid_ids)
 
 
-def part2(data):
+def part2(data: list[tuple[int, int]]) -> int:
     """Solve and return the answer to part 2."""
-    pass
+    invalid_ids = set()
+
+    for start, stop in data:
+        start_len = len(str(start))
+        stop_len = len(str(stop))
+
+        # Construct sub-ranges of similar length numbers: e.g. 1-9, 10-00, 100-999, 1_000-9_000, etc...
+        # but have it clamped to the overall start and stop ranges
+        for digit_len in range(start_len, stop_len + 1):
+            # Get all the factors of the digit length, besides the digit length itself
+            factors = [f for f in get_factors(digit_len) if f != digit_len]
+
+            # Generate every possible number with a length = factor
+            for f in factors:
+                n_digit_numbers = generate_n_len_digits(f)
+
+                for n in n_digit_numbers:
+                    # Repeat each number, and add it to the set of invalid_ids if it is within the boundary.
+                    # Stop processing if the number gets greater than the end of the original range.
+                    repeated_num = int(str(n) * (digit_len // f))
+                    if start <= repeated_num <= stop:
+                        invalid_ids.add(repeated_num)
+                    elif repeated_num > stop:
+                        break
+
+    return sum(invalid_ids)
 
 
 def solve(puzzle_input) -> tuple:
